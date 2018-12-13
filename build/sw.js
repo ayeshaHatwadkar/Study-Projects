ef926ccfae34afb9f5f2146f122963091f76ce90
ayeshaHatwadkar <45847787+ayeshaHatwadkar@users.noreply.github.com>
2018-12-13 16:46:17 +0530

    Add files via upload

---
+++ build/sw.js
@@ -0,0 +1,36 @@
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.0.0/workbox-sw.js');
+
+workbox.routing.registerRoute(
+    new RegExp('https://jsonplaceholder.typicode.com/users'),
+    workbox.strategies.cacheFirst()
+);
+
+workbox.routing.registerRoute(
+    new RegExp('https://fonts.(?:googleapis|gstatic).com/(.*)'),
+    workbox.strategies.cacheFirst({
+      cacheName: 'google-fonts',
+      plugins: [
+        new workbox.expiration.Plugin({
+          maxEntries: 30,
+        }),
+        new workbox.cacheableResponse.Plugin({
+          statuses: [0, 200]
+        }),
+      ],
+    }),
+  );
+
+workbox.precaching.precacheAndRoute([
+  {
+    "url": "css/main.css",
+    "revision": "d3072ab3693c185313018e404e07d914"
+  },
+  {
+    "url": "index.html",
+    "revision": "26a712662e4f06b3fcbb3d2a0d1ef9da"
+  },
+  {
+    "url": "js/app.js",
+    "revision": "6b73f45a2506a26e00e425688eaa6514"
+  }
+]);
